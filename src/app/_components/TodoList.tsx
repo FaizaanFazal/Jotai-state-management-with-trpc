7435f316ac2f2397deb31552fcfd19b10dda63c7
FaizaanFazal <dkfaizaan12@gmail.com>
2024-08-07 23:43:31 +0900

    added jotai

--- src/app/_components/TodoList.tsx
+++ src/app/_components/TodoList.tsx
@@ -1,5 +1,5 @@
 "use client";
-// import {  atom, useAtom } from 'jotai'
+import {  atom, useAtom } from 'jotai'
 import { useState } from "react";
 import { trpc } from "../_trpc/client";
 import { serverClient, UserGetUser } from "../_trpc/serverClient";
@@ -17,20 +17,23 @@ export default function TodoList({
     initialUsers: Users[] | undefined
 }) {
     const getUsers = trpc.users.getUsers.useQuery();
+    const idAtom = atom(getUsers)
+    const [data, refresh] = useAtom(idAtom)
     const addUser = trpc.users.addUser.useMutation({
         onSettled: () => {
-            getUsers.refetch();
+            getUsers.refetch()
         }
     });
+  
     const [name, setName] = useState<string>("");
     const [race, setRace] = useState<string>("");
     // const [data, refresh] = useAtom(queryAtom)
     // console.log(data)
     return (
-        <div>
+        <div className='w-[70%] mx-auto border-2 border-black'>
             {JSON.stringify(initialUsers)}
             <div>Initial users above</div>
-            {JSON.stringify(getUsers.data)}
+            {JSON.stringify(data.data)}
             <div className="flex flex-col gap-3">
                 Name: {" "}
                 <input
--- src/app/_trpc/jotaiClient.ts
+++ src/app/_trpc/jotaiClient.ts
@@ -1,15 +1,16 @@
 // import { atom } from 'jotai'
-import { createTRPCJotai } from 'jotai-trpc'
-import { httpLink } from '@trpc/client';
-import { AppRouter } from '@/server';
+// import { createTRPCJotai } from 'jotai-trpc'
+// import { httpLink } from '@trpc/client';
+// import { atomWithQuery } from 'jotai/query';
+// import { appRouter, type AppRouter } from "@/server";
 
-const jotaiClient = createTRPCJotai<AppRouter>({
-  links: [
-    httpLink({
-      url: "http://localhost:3000/api/trpc",
-    }),
-  ],
-})
+// const jotaiClient = createTRPCJotai<AppRouter>({
+//   links: [
+//     httpLink({
+//       url: "http://localhost:3000/api/trpc",
+//     }),
+//   ],
+// })
 
 // const idAtom = atom()
 // export const queryAtom = jotaiClient.users.getUsers.atomWithQuery()
